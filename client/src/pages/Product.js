bf81a9d1644ad605aa3cd7a504bdcc45a6573a68
rmeason <rme15a@acu.edu>
2020-12-28 17:20:02 -0600

    outline of content on product page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -4,7 +4,89 @@ import "./Product.css";
 const Product = () => (
 
     <div className="product">
-        <p>product page</p>
+        
+        <div class="work-feature-block row">
+            <div class="columns medium-7">
+                <img class="work-feature-block-image" src="https://placehold.it/600x400"/>
+            </div>
+            <div class="columns medium-5">
+                <h2 class="work-feature-block-header">Basic Elegance</h2>
+                <p>Red velvet ribbon on artificial evergreen wreath.</p>
+                <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                </ul>
+                <p>Red velvet ribbon on lighted, artificial evergreen wreath.</p>
+                <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="work-feature-block row">
+
+            <div class="columns medium-5">
+                <h2 class="work-feature-block-header">Sliding Gates</h2>
+                <p>If you have a gate that slides, we can "wrap" the gate.  We place bows at either side of the gate and run ribbon through the gate itself.</p>
+                <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                </ul>
+            </div>            
+            <div class="columns medium-7">
+                <img class="work-feature-block-image" src="https://placehold.it/600x400"/>
+            </div>
+        </div>
+
+
+        <div class="work-feature-block row">
+            
+            <div class="columns medium-7">
+                <img class="work-feature-block-image" src="https://placehold.it/600x400"/>
+            </div>
+            <div class="columns medium-5">
+                <h2 class="work-feature-block-header">Lighted Trees</h2>
+                <p>If you have trees that you would like wrapped with lights, we are on the job! We use a commercial grade, concave, LED bulb. They are super bright with a brown wire that is not as conspicuous on tree trunks. Contact us for pricing.</p>
+                <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="work-feature-block row">
+
+            <div class="columns medium-5">
+                <h2 class="work-feature-block-header">Christmas Party</h2>
+                <p>Red velvet ribbon on artificial green wreath with shatter-proof ornaments in one or two colors.</p>
+                <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                </ul>
+                <p>Red velvet ribbon on lighted, artificial green wreath with shatter-proof ornaments in one or two colors.</p>
+                <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                </ul>
+            </div>            
+            <div class="columns medium-7">
+                <img class="work-feature-block-image" src="https://placehold.it/600x400"/>
+            </div>
+        </div>
+
     </div>
 
 );
