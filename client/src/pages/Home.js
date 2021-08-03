db055c3aeede33519ec3aa4a3f7e328547422a43
rmeason <rme15a@acu.edu>
2021-08-03 16:28:06 -0500

    FavIcon and home page edits

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,7 +10,7 @@ function Home () {
     return (
 
     <div className="about-the-author">
-        <h1 className="author-title">About Us</h1>
+        
         <div className="row">
             <div className="small-12 medium-4 columns" id="aboutUsPic">
             <div className="author-image">
@@ -21,8 +21,8 @@ function Home () {
                 />
             </div>
             </div>
-            <div className="small-12 medium-8 columns" id="abtUs">
-            <h4 className="separator-left"></h4>
+            <div className="small-12 medium-8 columns" id="abtUs" style="text-align: left">
+            
                 <p>Maple Street Designs, LLC specializes in meeting the decorating needs of  property management companies as well as small business owners in and around the Houston, TX area.
                 We offer both ready-made and custom designs for rent or purchase for Christmas and other holidays and special occasions.  
                 We are committed to customer service and satisfaction.  Call today to see what we can do for you. 
